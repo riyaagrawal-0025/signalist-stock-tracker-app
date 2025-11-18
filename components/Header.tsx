39d1e130c120a5a8e1f7c9e6f05accdb2784bce0
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2025-11-18 20:01:07 +0530

    header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+import NavItems from './NavItems'
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 header'>
+        <div className='container header-wrapper'>
+          <Link href="/">
+            <Image src="/assests/icons/logo.svg" alt="Signalist logo" width={140} height={32} className='h-8 w-auto cursor-pointer'/></Link>
+            <nav className='hidden sm:block'><NavItems/></nav>
+        </div>
+    </header>
+  )
+}
+export default Header
